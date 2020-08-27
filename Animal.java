2de4deefe5bfed20ae0576e5d8cdd5daf4bf8c02
JackMasciopinto <john.masciopinto@gmail>
2020-08-27 10:30:16 -0400

    added animal

---
+++ Animal.java
@@ -0,0 +1,12 @@
+abstract class Animal{
+    private int age;
+    private String name, consStatus, repoMethod, food;
+    public Animal(int a,String n, String stat, String rep, String f){
+        this.age = a;
+        this.name = n;
+        this.consStatus = stat;
+        this.repoMethod = rep;
+        this.food = f;
+    }
+     
+}
