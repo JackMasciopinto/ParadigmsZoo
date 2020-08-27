ec558ff668c30afd84c42e5fe25f87a4867d8e38
JackMasciopinto <john.masciopinto@gmail>
2020-08-27 13:56:26 -0400

    hopefully finished animal class

--- Animal.java
+++ Animal.java
@@ -1,12 +1,28 @@
-abstract class Animal{
-    private int age;
-    private String name, consStatus, repoMethod, food;
-    public Animal(int a,String n, String stat, String rep, String f){
+public abstract class Animal{
+
+    protected int age;
+    protected String name;
+    protected String consStatus;
+    protected String repoMethod;
+    protected String food;
+
+    public Animal(int a,String n){
         this.age = a;
         this.name = n;
-        this.consStatus = stat;
-        this.repoMethod = rep;
-        this.food = f;
     }
-     
+    
+    public String getName() { return this.name; }    
+    public void setName(String n) { this.name = n; }
+
+    public int getAge() { return this.age }
+    public void setAge(int a) { this.age = a; }
+
+    public String getStatus { return this.consStatus; }
+    
+    public String getFood() { return this.food }
+    public void setFood(String f) { this.food = f }
+
+    public String getRepo() { return this.repoMethod } 
+
+    public abstract void display();
 }
